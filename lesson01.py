eadac4408ebcc5021ea87ebbaa52cd6fdc34dbf8
Anna Tiscenko <anna.tiscenko@gmail.com>
2020-10-24 02:41:38 +0300

    for loop

--- lesson01.py
+++ lesson01.py
@@ -1,1 +1,21 @@
 print('Privet Oleg')
+print(2 + 2)
+
+print('privet ' * 69)
+
+a = 2 + 3
+print(a)
+
+b = [1, 2, 7, 'a']
+
+for i in b:
+    print(i)
+    print('popka')
+
+g = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for m in g:
+    print(m)
+
+for i in range(100):
+    print(i)
